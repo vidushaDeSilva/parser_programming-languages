a92e2dd8b044b9984a14ee2694643d21e2c8cb6b
vidushaDeSilva <vidusha.21@cse.mrt.ac.lk>
2024-03-29 17:07:55 +0530

    tokenize

--- lex analyzer.py
+++ lex analyzer.py
@@ -1,18 +1,23 @@
 class lex:
     def __init__(self, input):
         self.input = input
-        self.pos = 0
-        self.tokens = []
+        self.pos = -1
+        self.current_char = None
+        self.operator_symbol = ['+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '<', '>', '=', '!', '&', '|']
         self.digits = '0123456789'
-        self.ops = ['+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '<', '>', '=', '!', '&', '|']
+        self.punctuation = ['(', ')', ';', ',']
+        self.tokens = []
 
-    
 
+
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.input[self.pos] if self.pos < len(self.input) else None
+            
     def lex(self):
         while self.pos < len(self.input):
             if self.input[self.pos] in self.digits:
-                self.token.append('INT', self.input[self.pos])
-                self.pos += 1
+                self.tokenize_INTEGERS()
             elif self.input[self.pos] in self.ops:
                 if self.input[self.pos] == '+':
                     self.token.append(['PLUS',self.input[self.pos]])
@@ -26,6 +31,43 @@ class lex:
                     self.token.append(['AND', self.input[self.pos]])
                 elif self.input[self.pos] == '|':
                     self.token.append(['OR', self.input[self.pos]])
+            elif self.input[self.pos] in self.punctuation:
+                if self.input[self.pos] == '(':
+                    self.token.append(['LPAREN', self.input[self.pos]])
+                elif self.input[self.pos] == ')':
+                    self.token.append(['RPAREN', self.input[self.pos]])
+                elif self.input[self.pos] == ';':
+                    self.token.append(['SEMI', self.input[self.pos]])
+                elif self.input[self.pos] == ',':
+                    self.token.append(['COMMA', self.input[self.pos]])
+            elif self.input[self.pos].isalpha():
+                self.tokenize_ID()
+                
+
+
+    def tokenize_INTEGERS(self):
+        while self.pos < len(self.input):
+            if self.input[self.pos] in self.digits:
+                token = self.input[self.pos]
+                self.pos += 1
+                while self.pos < len(self.input) and self.input[self.pos] in self.digits:
+                    token += self.input[self.pos]
+                    self.pos += 1
+                self.tokens.append(('INTEGER', token))
+            else:
+                self.pos += 1
+
+    def tokenize_ID(self):
+        while self.pos < len(self.input):
+            if self.input[self.pos].isalpha():
+                token = self.input[self.pos]
+                self.pos += 1
+                while self.pos < len(self.input) and (self.input[self.pos].isalpha() or self.input[self.pos].isdigit()):
+                    token += self.input[self.pos]
+                    self.pos += 1
+                self.tokens.append(('ID', token))
+            else:
+                self.pos += 1
                 
             
         
