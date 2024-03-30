9940dcdf39309fa4177f0d432e33b226dc292026
vidushaDeSilva <vidusha.21@cse.mrt.ac.lk>
2024-03-30 21:35:31 +0530

    lexer

--- lex analyzer.py
+++ lex analyzer.py
@@ -1,82 +1,81 @@
 class lex:
     def __init__(self, input):
         self.input = input
-        self.pos = -1
-        self.current_char = None
+        self.pos = 0
         self.operator_symbol = ['+', '-', '*', '/', '(', ')', '{', '}', '[', ']', ';', ',', '<', '>', '=', '!', '&', '|']
         self.digits = '0123456789'
         self.punctuation = ['(', ')', ';', ',']
         self.tokens = []
 
-            
     def lex(self):
         while self.pos < len(self.input):
             if self.input[self.pos] in self.digits:
                 self.tokenize_INTEGERS()
-            elif self.input[self.pos] in self.ops:
-                self.token.append(['OPERATOR', self.input[self.pos]])
+            elif self.input[self.pos] in self.operator_symbol:
+                self.tokens.append(['OPERATOR', self.input[self.pos]])
+                self.pos += 1
             elif self.input[self.pos] in self.punctuation:
-                if self.input[self.pos] == '(':
-                    self.token.append(['LPAREN', self.input[self.pos]])
-                elif self.input[self.pos] == ')':
-                    self.token.append(['RPAREN', self.input[self.pos]])
-                elif self.input[self.pos] == ';':
-                    self.token.append(['SEMI', self.input[self.pos]])
-                elif self.input[self.pos] == ',':
-                    self.token.append(['COMMA', self.input[self.pos]])
+                self.tokens.append(['PUNCTUATION', self.input[self.pos]])
+                self.pos += 1
             elif self.input[self.pos].isalpha():
                 self.tokenize_ID()
-            elif self.input[self.pos] == ' " ':
+            elif self.input[self.pos] == '"':
                 self.tokenize_STRING()
             elif self.input[self.pos] == ' ':
                 self.tokenize_DELETE_space()
             elif self.input[self.pos] == '/*':
                 self.tokenize_DELETE_comments()
+            else:
+                self.illegalCharError(self.input[self.pos])
+                self.pos += 1
+
+        return self.tokens
 
     def tokenize_INTEGERS(self):
-        while self.pos < len(self.input):
-                token = self.input[self.pos]
-                self.pos += 1
-                while self.pos < len(self.input) and self.input[self.pos] in self.digits:
-                    token += self.input[self.pos]
-                    self.pos += 1
-                self.tokens.append(('INTEGER', token))
+        token = self.input[self.pos]
+        self.pos += 1
+        while self.pos < len(self.input) and self.input[self.pos] in self.digits:
+            token += self.input[self.pos]
+            self.pos += 1
+        self.tokens.append(('INTEGER', token))
             
-
     def tokenize_ID(self):
-        while self.pos < len(self.input):
-                token = self.input[self.pos]
-                self.pos += 1
-                while self.pos < len(self.input) and (self.input[self.pos].isalpha() or self.input[self.pos].isdigit()):
-                    token += self.input[self.pos]
-                    self.pos += 1
-                self.tokens.append(('IDENTIFIER', token))
+        token = self.input[self.pos]
+        self.pos += 1
+        while self.pos < len(self.input) and self.input[self.pos].isalpha():
+            token += self.input[self.pos]
+            self.pos += 1
+        self.tokens.append(('IDENTIFIER', token))
 
     def tokenize_STRING(self):
-        while self.pos < len(self.input):
-                token = self.input[self.pos]
-                self.pos += 1
-                while self.pos < len(self.input) and self.input[self.pos] != '"':
-                    token += self.input[self.pos]
-                    self.pos += 1
-                token += self.input[self.pos]
-                self.tokens.append(('STRING', token))
+        token = self.input[self.pos]
+        self.pos += 1
+        while self.pos < len(self.input) and self.input[self.pos] != '"':
+            token += self.input[self.pos]
+            self.pos += 1
+        token += self.input[self.pos]
+        self.tokens.append(('STRING', token))
 
     def tokenize_DELETE_space(self):
-        while self.pos < len(self.input):
-                token = self.input[self.pos]
-                self.pos += 1
-                while self.pos < len(self.input) and self.input[self.pos] != ' ':
-                    token += self.input[self.pos]
-                    self.pos += 1
-                self.tokens.append(('DELETE', token))
+        token = self.input[self.pos]
+        self.pos += 1
+        while self.pos < len(self.input) and self.input[self.pos] == ' ':
+            token += self.input[self.pos]
+            self.pos += 1
+        self.tokens.append(('DELETE', token))
 
     def tokenize_DELETE_comments(self):
-        while self.pos < len(self.input):
-                token = self.input[self.pos]
-                self.pos += 1
-                while self.pos < len(self.input) and self.input[self.pos] != '*/':
-                    token += self.input[self.pos]
-                    self.pos += 1
-                token += self.input[self.pos]
-                self.tokens.append(('DELETE', token))
+        token = self.input[self.pos]
+        self.pos += 1
+        while self.pos < len(self.input) and self.input[self.pos] != '*/':
+            token += self.input[self.pos]
+            self.pos += 1
+        token += self.input[self.pos]
+        self.tokens.append(('DELETE', token))
+
+    def illegalCharError(self, char):
+         print(char+" is an illegal character")
+
+lexer = lex("sas136*        123")
+tokens = lexer.lex()
+print(tokens)
